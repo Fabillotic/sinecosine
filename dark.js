538b5183ed695c0f4f78d4f9d2dedb06278a1d6a
Fabillotic <fabillo552@gmail.com>
2024-12-05 15:48:17 +0100

    Change dark mode functionality, set light default

--- dark.js
+++ dark.js
@@ -1,16 +1,22 @@
 window.dark = false;
 
-function darkMode() {
+function toggleDarkMode() {
 	window.dark = !window.dark;
+	updateDarkMode();
+}
 
+function updateDarkMode() {
 	let headline = document.getElementById("headline");
+	let button = document.getElementById("dark");
 
 	if(window.dark) {
 		headline.style.color = "white";
 		document.body.style.background = "black";
+		button.innerHTML = "Light mode";
 	}
 	else {
 		headline.style.color = "black";
 		document.body.style.background = "white";
+		button.innerHTML = "Dark mode";
 	}
 }
